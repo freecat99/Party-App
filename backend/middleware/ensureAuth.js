147d57e46c0b24fafc0c810653f618d4e6b8ead6
adesh <adesh9744@gmail.com>
2025-08-22 03:20:00 +0530

    Add feature

--- backend/middleware/ensureAuth.js
+++ backend/middleware/ensureAuth.js
@@ -4,15 +4,15 @@ const cascadeAuth = (req, res, next) => {
     const auth = req.headers['authorization'];
 
     if(!auth){
-        res.status(401).json({"message":"Unauthorized Access"});
+        return res.status(401).json({"message":"Unauthorized Access"});
     }
 
     try{
         const decode = jwt.verify(auth, process.env.JWT);
         req.user = decode;
-        next();
+        return next();
     }catch(err){
-        res.status(403).json({"message":`Token expired/ wrong, ${err}`})
+        return res.status(403).json({"message":`Token expired/ wrong, ${err}`})
     }
 };
 
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { handleDefault, handleFailure } from '../utils';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function Home(){
+    
+    const [loggedInUser, setLoggedInUser] = useState('');
+    const [party, setParty] = useState([]);
+    const navigate = useNavigate();
+    
+    useEffect(()=>{
+        setLoggedInUser(localStorage.getItem('loggedInAs'))
+    },[]);
+    
+    const handleLogout = (e) =>{
+        handleDefault("We will miss you!");
+        localStorage.removeItem('loggedInAs');
+        localStorage.removeItem('token');
+
+        setTimeout(() => {
+            navigate('/login');
+        }, 2000);
+    }
+    
+    const fetchParty = async()=>{
+        try{
+            const url = "http://localhost:1601/party";
+            const headers = {
+                headers:{
+                    'Authorization': localStorage.getItem('token')
+                }
+            }
+            const response = await fetch(url, headers);
+            const result = await response.json();
+            
+            setParty(result);
+        }catch(err){
+            handleFailure(err);
+        }
+    }
+
+    useEffect(()=>{
+        fetchParty();
+    },[])
+
     return(
-        <h1>Home</h1>
+        <div className='container'>
+            <h1>{loggedInUser}</h1>
+            <button onClick={handleLogout}>Logout</button>
+            <div>
+                
+                {party.map((item, index)=>(
+                    <ul key={index}>
+                        <span>{item.partyId} with {item.name}</span>
+                    </ul>
+                ))}
+                
+            </div>
+            <ToastContainer/>
+        </div>
     );
 }
 
