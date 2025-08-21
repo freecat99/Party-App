e145a87987bdfc898860b029fc0cec16d4f3b9a6
freecat99 <adesh9744@gmail.com>
2025-08-21 21:00:36 +0530

    Added frontend-backend connection

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const app = express();
 
 require('./models/db')
 
 require('dotenv').config();
 
+const corsOptions = {
+  origin: '*',
+};
+
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 1600;
-const app = express();
 
 
 app.use(bodyParser.json());
-app.use(cors());
 
 app.use('/auth', require('./router/authRouter'))
 app.use('/party', require('./router/partyRouter'))
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,24 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { handleFailure } from '../utils';
 
 function Signup(){
+
+    const [signUpInfo, setSignUpInfo] = useState({
+        name:'',
+        email:'',
+        password:''
+    });
+
+    const handleChange=(e)=>{
+        const {name, value} = e.target;
+        console.log(name, value);
+        const copySignUpInfo = {...signUpInfo};
+        copySignUpInfo[name] = value;
+        setSignUpInfo(copySignUpInfo);
+    }
+    console.log(signUpInfo); 
+
+    const handleSignUp = async(e) => {
+        e.preventDefault();
+
+        const {name, email, password} = signUpInfo;
+        if(!name || !email || !password){
+            return handleFailure("all fields necessary!")
+        }
+
+        try{
+            const url = "http://localhost:1601/auth/signup";
+            const response = await fetch(url,{
+                method:"POST",
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(signUpInfo)
+            })
+            const result = await response.json();
+            console.log(result);
+            
+        }catch(err){
+            handleFailure(err);
+        }
+    }
+
+
     return(
         <div className='container'>
             <h1>Signup</h1>
-                <form>
+                <form onSubmit={handleSignUp}>
                     <div>
                         <label htmlFor='name'>Name</label>
-                        <input type='text' name='name' autoFocus placeholder='name here!' />
+                        <input onChange={handleChange} type='text' name='name' autoFocus placeholder='name here!' />
                     </div>
                     <div>
                         <label htmlFor='email'>Email</label>
-                        <input type='email' name='email' placeholder='email here!' />
+                        <input onChange={handleChange} type='email' name='email' placeholder='email here!' />
                     </div>
                     <div>
                         <label htmlFor='password'>Password</label>
-                        <input type='password' name='password' placeholder='password here!' />
+                        <input onChange={handleChange} type='password' name='password' placeholder='password here!' />
                     </div>
                     <button type='submit'>Register</button>
                     <span>Already have an account? <Link className='linkStyle' to="/login">Login</Link> </span>
