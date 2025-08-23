c68e2c8bd244ea64cf4045b4f06723c6e0f04ac9
freecat99 <adesh9744@gmail.com>
2025-08-23 21:00:08 +0530

    Added Refresh Handler

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react'
-import { Navigate, Route, Router, Routes } from 'react-router-dom';
-import Home from './components/Home';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/Home';
+import { useState } from 'react';
+import RefreshHandler from './RefreshHandler';
 
 function App() {
-  return(
-    <div className='App'>
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
+
+  return (
+    <div className="App">
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
-        <Route path='/' element= {<Navigate to = "/login"/>}></Route>
-        <Route path='/home' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
       </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
